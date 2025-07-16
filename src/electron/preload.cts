d9bead8f0221f2dc6fdcce04acd2e914df12529e
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2025-07-16 12:04:25 -0500

    Create IPC bridge functions and types

--- src/electron/preload.cts
+++ src/electron/preload.cts
@@ -1,5 +1,18 @@
-import { contextBridge, ipcRenderer } from "electron";
+import electron from "electron";
 
-contextBridge.exposeInMainWorld("electron", {
-    test: () => console.log("This works"),
-});
+electron.contextBridge.exposeInMainWorld("electron", {
+    getProducts: () => ipcInvoke("getProducts"),
+    getCombos: () => ipcInvoke("getCombos"),
+    authenticatePIN: (pin) => ipcInvoke("authenticatePIN", pin),
+    startSession: (user) => ipcInvoke("startSession", user),
+    endSession: (user) => ipcInvoke("endSession", user),
+    createSale: (sale) => ipcInvoke("createSale", sale),
+} satisfies Window["electron"]);
+
+function ipcInvoke<Key extends keyof EventPayloadMapping>(
+    ...[key, payload]: EventPayloadMapping[Key]["request"] extends undefined
+        ? [key: Key]
+        : [key: Key, payload: EventPayloadMapping[Key]["request"]]
+): Promise<EventPayloadMapping[Key]["response"]> {
+    return electron.ipcRenderer.invoke(key, payload);
+}
---
+++ types.d.ts
@@ -0,0 +1,88 @@
+type User = {
+    id: number;
+    username: string;
+    first_name: string;
+    last_name: string;
+    pin: number;
+};
+
+type Product = {
+    id: number;
+    name: string;
+    type: string;
+    default_price: number;
+    image_path: string;
+};
+
+type Combo = {
+    id: number;
+    name: string;
+    default_price: number;
+    items: { product: Product; quantity: number }[];
+};
+
+type Sale = {
+    id?: number;
+    total: number;
+    user_id: number;
+    time: string;
+    items: SaleItem[];
+};
+
+type SaleItem = {
+    id?: number;
+    sale_id?: number;
+    product_id: number | null;
+    combo_id: number | null;
+    sale_price: number;
+    price_modified: boolean;
+    price_modified_by: number | null;
+};
+
+type Session = {
+    id?: number;
+    start: string;
+    started_by: number;
+    end: string | null;
+    ended_by: number | null;
+    description: string | null;
+    sales: Sale[];
+};
+
+type EventPayloadMapping = {
+    getProducts: {
+        request: undefined;
+        response: Product[];
+    };
+    getCombos: {
+        request: undefined;
+        response: Combo[];
+    };
+    authenticatePIN: {
+        request: number;
+        response: User | null;
+    };
+    startSession: {
+        request: User;
+        response: Session;
+    };
+    endSession: {
+        request: User;
+        response: Session;
+    };
+    createSale: {
+        request: Sale;
+        response: boolean;
+    };
+};
+
+interface Window {
+    electron: {
+        getProducts: () => Promise<Product[]>;
+        getCombos: () => Promise<Combo[]>;
+        authenticatePIN: (pin: number) => Promise<User | null>;
+        startSession: (user: User) => Promise<Session>;
+        createSale: (sale: Sale) => Promise<boolean>;
+        endSession: (user: User) => Promise<Session>;
+    };
+}
