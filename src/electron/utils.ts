ac7c3c41c6edbf78754b7e92c82c937706d14819
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2025-07-16 23:59:39 -0500

    Add helper function for type safe ipcMain.handle

--- src/electron/utils.ts
+++ src/electron/utils.ts
@@ -1,4 +1,4 @@
-import { app } from "electron";
+import { app, ipcMain } from "electron";
 import path from "path";
 
 export function isDev() {
@@ -10,4 +10,18 @@ export function getDbPath() {
         return path.join(app.getAppPath() + "/src/electron/database/dev.db");
     }
     return path.join(app.getPath("appData"), "pos.db");
+}
+
+export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
+    key: Key,
+    handler: (
+        ...[event, payload]: EventPayloadMapping[Key]["request"] extends undefined
+            ? []
+            : [event: Electron.IpcMainInvokeEvent, payload: EventPayloadMapping[Key]["request"]]
+    ) => EventPayloadMapping[Key]['response']
+) {
+    //I'm casting as any here because TS can't resolve the conditional tuple down to the 
+    //expected shape. ChatGPT seems to think this is legitimate. I'm not so sure, but I don't
+    //any better...
+    ipcMain.handle(key, handler as any); 
 }
