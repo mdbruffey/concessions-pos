6c2449f4daaaf88427415f5b5c6948fe252f7863
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2025-08-08 15:47:47 -0500

    Remove old product populating

--- src/electron/controllers.ts
+++ src/electron/controllers.ts
@@ -3,12 +3,6 @@ import db from "./database/db.js";
 export function getProducts(): Product[] {
     const allProducts = db.prepare<[], Product>("SELECT * from products");
     const products = allProducts.all();
-    if (products.length === 0) {
-        const entry = db.prepare(
-            "INSERT into products (name, type, default_price) VALUES(?,?,?)"
-        );
-        entry.run("Hotdog", "Hot", 2.0);
-    }
     return products;
 }
 
