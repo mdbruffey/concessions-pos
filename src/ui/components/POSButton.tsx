e2938790115f94d4533fd20815445407e9d2a6e0
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2026-03-02 16:02:38 -0600

    Add backgroundColor prop

--- src/ui/components/POSButton.tsx
+++ src/ui/components/POSButton.tsx
@@ -4,16 +4,21 @@ type ItemButtonProps = {
     label: string;
     imagePath?: string;
     className?: string;
+    backgroundColor?: string;
     onClick: (e: React.SyntheticEvent<HTMLButtonElement>) => void;
 };
 
 export default function POSButton({
     label,
     className,
+    backgroundColor,
     onClick,
 }: ItemButtonProps) {
+    let style = {};
+    if (backgroundColor) style = { backgroundColor: backgroundColor };
     return (
         <button
+            style={style}
             onClick={(e) => e.stopPropagation()}
             className={(className ? className + " " : "") + styles.itemButton}
             onPointerDown={(e) => e.currentTarget.classList.add("pressed")}
