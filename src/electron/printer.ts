093906fb8c2f7e54fbcaeccfebf89b644190bd35
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2026-03-02 12:27:32 -0600

    Catch another error when no printer connected

--- src/electron/printer.ts
+++ src/electron/printer.ts
@@ -8,7 +8,7 @@ try{
     device = new USB();
     printer = new escpos.Printer(device);
 }
-catch(error) {console.log(error)}
+catch(error) {console.log("Error finding printer.")}
 
 export function printText(text: string) {
     let ran = false
@@ -28,6 +28,10 @@ export function printText(text: string) {
 
 export function printReceipt(receipt: Receipt){
     let ran = false
+    if(!device) {
+        console.log("No print device connected.")
+        return ran
+    }
     device.open(() => {
         if (!ran){
             for (let i = 0; i < 2; i++){
